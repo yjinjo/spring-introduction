c735f30c6c4736a415efc4b9df168c00487efea7
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-27 17:59:23 +0900

    #3. 회원 서비스 개발

---
+++ src/main/java/hello/springintroduction/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.springintroduction.service;
+
+import hello.springintroduction.domain.Member;
+import hello.springintroduction.repository.MemberRepository;
+import hello.springintroduction.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입을 하면 id만 리턴하기로 합니다.
+     *   - 같은 이름이 있는 회원이 있으면 안됩니다.
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 특정 회원 조회
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
