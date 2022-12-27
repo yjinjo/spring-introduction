fd5fc45cb8f001ce2f82f37a7d8e24b57c864536
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-27 23:49:32 +0900

    #6. 순수 JDBC

--- src/main/java/hello/springintroduction/SpringConfig.java
+++ src/main/java/hello/springintroduction/SpringConfig.java
@@ -1,13 +1,19 @@
 package hello.springintroduction;
-
+import hello.springintroduction.repository.JdbcMemberRepository;
+//import hello.springintroduction.repository.JdbcTemplateMemberRepository;
 import hello.springintroduction.repository.MemberRepository;
 import hello.springintroduction.repository.MemoryMemberRepository;
 import hello.springintroduction.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +22,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
