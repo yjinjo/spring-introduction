5f2a26a7c71474214731e7eef169d8d27819b820
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-28 15:33:22 +0900

    #6. JPA

--- src/main/java/hello/springintroduction/SpringConfig.java
+++ src/main/java/hello/springintroduction/SpringConfig.java
@@ -1,18 +1,25 @@
 package hello.springintroduction;
-import hello.springintroduction.repository.JdbcMemberRepository;
-import hello.springintroduction.repository.JdbcTemplateMemberRepository;
-import hello.springintroduction.repository.MemberRepository;
-import hello.springintroduction.repository.MemoryMemberRepository;
+import hello.springintroduction.repository.*;
 import hello.springintroduction.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
+//    private final DataSource dataSource;
+//
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -24,6 +31,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
